6a09b6d6a18f536368e88cd381bb6982c0108a09
omeraydmr <osilix1@gmail.com>
2021-04-25 17:48:19 +0300

    second lesson : class and constructor

---
+++ oopIntro/src/oopIntro/Product.java
@@ -0,0 +1,25 @@
+package oopIntro;
+
+public class Product {
+	
+	//Constructor
+	public Product() {
+		System.out.println("Ben Çalýþtým");
+	}
+	
+	//Overloading
+	public Product(int id, String name, double unitPrice, String detail) {
+		this();
+		this.id = id;
+		this.name = name;
+		this.unitPrice = unitPrice;
+		this.detail = detail;
+	}
+	
+	int id;
+	String name;
+	double unitPrice;
+	String detail;
+	
+
+}
